a54e6ccfa3261d4585662930cb2c4206f8d727c7
alvinhunter7450 <hunteral21@students.ecu.edu>
2025-01-30 13:12:24 -0500

    add logo

---
+++ budget-app/src/App.js
@@ -0,0 +1,12 @@
+import cover from './cover.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={cover} alt="Cover" className="Cover"/>
+    </div>
+  );
+}
+
+export default App;
