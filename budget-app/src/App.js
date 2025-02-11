f783ca910803f09be4142972e0d6fb6e935ea9a1
hunter7450 <alvinhunter252@gmail.com>
2025-02-11 10:53:35 -0500

    starts of a login page

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -1,11 +1,45 @@
 import cover from './cover.svg';
 import './App.css';
+import { useForm } from "react-hook-form"
+import React from 'react';
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors},
+  } = useForm();
+  
+  const onSubmit = (data) => {
+    const userData = JSON.parse(localStorage.getItem(data.email));
+    if (userData){
+      if (userData.password === data.password) {
+        console.log(userData.name + "You Are Successfully Logged In");
+      } else {
+          console.log("Email or Password is not matching with our record");
+      }
+    } else {
+        console.log("Email or Password is not matching with our record");
+    }
+  };
   return (
+    // React Fragments
+    <>
     <div className="App">
       <img src={cover} alt="Cover" className="Cover"/>
     </div>
+
+    <form className="Login" onSubmit={handleSubmit(onSubmit)}>
+      <input type="email" {...register("email", {required: true})}/>
+      {errors.email && <span style={{color: "red"}}>
+        *Email* is mandatory </span>}
+      <input type="password" {...register("password")} />
+      <input type={"submit"}
+        style={{ backgroundColor : "#0F3D91"}} />
+
+    </form>
+
+    </>
   );
 }
 
